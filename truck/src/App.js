4a60a1d64afb234fa39e000e2d8774784df79e77
Tejasvee Dighe <tejasveedighe@gmail.com>
2023-03-03 12:39:37 +0530

    Basic Trucker Page form

---
+++ truck/src/App.js
@@ -0,0 +1,34 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useState } from "react";
+
+function App() {
+	const [lat, setLat] = useState();
+	const [lng, setLng] = useState();
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+	return (
+		<div className="App App-header">
+			<form>
+				<label htmlFor="lat">Latitude</label>
+				<input
+					id="lat"
+					value={lat}
+					placeholder={"Enter Latitude"}
+					onChange={(event) => setLat(event.currentTarget.value)}
+				/>
+				<br />
+				<label htmlFor="lng">Longitude</label>
+				<input
+					id="lng"
+					value={lng}
+					placeholder={"Enter Longitude"}
+					onChange={(event) => setLng(event.currentTarget.value)}
+				/>
+			</form>
+		</div>
+	);
+}
+
+export default App;
